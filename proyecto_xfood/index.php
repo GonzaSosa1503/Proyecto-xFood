d2e710e3e7026ad534f6da2dec273dab7e9d6afd
GonzaSosa1503 <145379729+GonzaSosa1503@users.noreply.github.com>
2023-11-01 08:19:03 -0300

    Logo actualizadou

--- proyecto_xfood/index.php
+++ proyecto_xfood/index.php
@@ -7,7 +7,7 @@
         <meta name="author" content="" />
         <title>xFood Hamburguesas y mas</title>
         <!-- Favicon-->
-        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <link rel="icon" type="image/x-icon" href="./img/xFood.png" />
         <!-- Bootstrap icons-->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet" />
         <!-- Core theme CSS (includes Bootstrap)-->
@@ -17,7 +17,7 @@
         <!-- Responsive navbar-->
         <nav class="navbar navbar-expand-lg navbar-dark black border-bottom border-warning">
             <div class="container px-lg-5 bs">
-                <a class="navbar-brand text-warning" href="#!">xFood.com</a>
+                <a class="navbar-brand text-warning" href="#!"><img src="./img/xFood.png" height="32px"></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
